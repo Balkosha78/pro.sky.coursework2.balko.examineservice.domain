a4719f5d0e1d0688212db0af10ed983a4648bd14
Алексей <balko-aleksey@yandex.ru>
2023-07-01 20:15:26 +0700

    initial commit

---
+++ src/main/java/pro/sky/coursework2/balko/examineservice/domain/Coursework2BalkoExamineserviceDomainApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2.balko.examineservice.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2BalkoExamineserviceDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework2BalkoExamineserviceDomainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/coursework2/balko/examineservice/domain/Coursework2BalkoExamineserviceDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.coursework2.balko.examineservice.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework2BalkoExamineserviceDomainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
